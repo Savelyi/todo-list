427e07af22112d987cd1172a52b74b41fa6eb07a
Savelyi <92672933+Savelyi@users.noreply.github.com>
2022-12-29 12:09:38 +0300

    Base Implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />)
+const store = createStore(todoApp)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
