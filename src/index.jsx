3925c37b39a4fe84af6593ec22afa321d20d4b28
Savelyi <savasvirsky@gmail.com>
2023-01-01 17:44:59 +0300

    Eslint refactoring

---
+++ src/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { createGlobalStyle } from 'styled-components';
+import todoApp from './reducers';
+import App from './components/App/App';
+
+const store = createStore(todoApp);
+
+const Global = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing:border-box;
+  font-size: 1.6rem;
+  font-family: 'Segoe UI';
+}
+`;
+render(
+  <div>
+    <Provider store={store}>
+      <Global />
+      <App />
+    </Provider>
+  </div>,
+
+  document.getElementById('root')
+);
