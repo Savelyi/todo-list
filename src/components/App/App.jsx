c9dd126147064e275afd4fdbd7f5035344de2b3a
Savelyi <savasvirsky@gmail.com>
2023-01-02 15:22:17 +0300

    Adding UseDispatch useSelector Hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Footer from '../Footer';
 import AddTodo from '../../containers/AddTodo';
-import VisibleTodoList from '../../containers/VisibleTodoList';
+import TodoList from '../TodoList/index';
 import { ToDoListWrapper } from './styled';
 
 function App() {
   return (
     <ToDoListWrapper>
       <AddTodo />
-      <VisibleTodoList />
+      <TodoList />
       <Footer />
     </ToDoListWrapper>
   );
--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,27 +1,46 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
 import Todo from '../Todo';
 import { TodosListWrapper } from './styled';
+import { VisibilityFilters } from '../../actions/VisibilityFilters';
+import { toggleTodo } from '../../actions/toggleTodo';
 
-function TodoList({ todos, toggleTodo }) {
+function TodoList() {
+  const dispatch = useDispatch();
+  const visibilityFilter = useSelector((state) => state.filter.filter);
+  const todosList = useSelector((state) => state.todos.todosList);
+
+  const getVisibleTodos = (todos, filter) => {
+    switch (filter) {
+      case VisibilityFilters.SHOW_ALL:
+        return todos;
+      case VisibilityFilters.SHOW_COMPLETED:
+        return todos.filter((t) => t.completed);
+      case VisibilityFilters.SHOW_ACTIVE:
+        return todos.filter((t) => !t.completed);
+      default:
+        throw new Error(`Unknown filter: ${filter}`);
+    }
+  };
+
   return (
     <TodosListWrapper>
-      {todos.map((todo) => (
-        <Todo key={todo.id} {...todo} onClick={() => toggleTodo(todo.id)} />
+      {getVisibleTodos(todosList, visibilityFilter).map((todo) => (
+        <Todo key={todo.id} {...todo} onClick={() => dispatch(toggleTodo(todo.id))} />
       ))}
     </TodosListWrapper>
   );
 }
 
-TodoList.propTypes = {
-  todos: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      completed: PropTypes.bool.isRequired,
-      text: PropTypes.string.isRequired
-    }).isRequired
-  ).isRequired,
-  toggleTodo: PropTypes.func.isRequired
-};
+// TodoList.propTypes = {
+//   todos: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       completed: PropTypes.bool.isRequired,
+//       text: PropTypes.string.isRequired
+//     }).isRequired
+//   ).isRequired,
+//   toggleTodo: PropTypes.func.isRequired
+// };
 
 export default TodoList;
